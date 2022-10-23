2c29f3581d425e91649c54aa81f95208177b268a
Nate-Kleinsorge <110139749+Nate-Kleinsorge@users.noreply.github.com>
2022-10-23 14:58:14 -0400

    added files

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+//set current date
+var now = moment().format("dddd, MMMM Do YYYY");
+document.getElementById("current-date").innerHTML = now;
+//get location coordinates by city name
+var city = "detroit"
+var apid = "5bcb706be88d247e41f5a053ad2b527b"
+var loc = "api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apid;
+
+
+
+
+// var weather = "api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apid;
+
+fetch(loc)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    });
