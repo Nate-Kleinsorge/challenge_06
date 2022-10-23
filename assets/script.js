bc85c4335d37100cdf25219eea23e7f59fca2895
Nate-Kleinsorge <110139749+Nate-Kleinsorge@users.noreply.github.com>
2022-10-23 15:10:09 -0400

    added more javascript

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,10 @@ var loc = "api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" 
 
 // var weather = "api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apid;
 
-fetch(loc)
+fetch(loc,{
+    method: "GET",
+    headers: {}
+})
     .then(function (response) {
     return response.json();
     })
